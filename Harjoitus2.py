64c0ce31004ea7b9ccc1e4778f15f25c6c8467ca
jefishub <yhhyhyy@hotmail.com>
2020-10-06 09:53:12 +0300

    kokonaisarvosana

--- Harjoitus2.py
+++ Harjoitus2.py
@@ -4,6 +4,7 @@ class Opiskelija:
         self.opiskelijanumero = numero
         self.tenttiarvosana = tenttiarvosana
         self.harjoitusarvosana = harjoitusarvosana
+        self.kokonais_arvosana = 0
     
     def tentti_suoritettu(self):
         if self.tenttiarvosana >= 3:
@@ -25,6 +26,14 @@ class Opiskelija:
         else:
             print("Ei kelvollinen arvosana")
 
+    def laske_kokonais_arvosana(self):
+        if self.tentti_suoritettu == 0 or self.harjoitusarvosana == 0:
+            self.kokonais_arvosana = 0
+            return self.kokonais_arvosana
+        else:
+            self.kokonais_arvosana = (1 + self.harjoitusarvosana + self.tenttiarvosana) / 3
+            return self.kokonais_arvosana
+
 
 kurssilainen1 = Opiskelija("Johan Kämp", "212233", 3, 5)
 kurssilainen2 = Opiskelija("Matti Korhonen", "562233", 2, 5)
@@ -37,3 +46,5 @@ kurssilainen2.vaihda_arvosana(5)
 print(kurssilainen2.tenttiarvosana)
 kurssilainen2.vaihda_arvosana(10)
 print(kurssilainen2.tenttiarvosana)
+
+print(f"Kokonaisarvosana on {kurssilainen2.laske_kokonais_arvosana()}")
