1d98f0b468933ec11598ad0b8aad61c7c995dad4
jefishub <yhhyhyy@hotmail.com>
2020-10-06 13:49:16 +0300

    Last fixes

--- Excercise1.py
+++ Excercise1.py
@@ -5,9 +5,11 @@ implementaatiota ja luo muutama oliota.
 
 Lisähaaste: Syntymäpäivä voi määritellä datetime muodossa ja kirjoita
 metodin joka laske henkilön iän."""
+
+
 from datetime import *
 
-class henkilo_luettelo:
+class Henkilo_luettelo:
     def __init__(self,nimi,sukunimi,syntpvm,email="",puhelinnumero="",kotiosoite=""):
         self.nimi = nimi
         self.sukunimi = sukunimi
@@ -15,12 +17,19 @@ class henkilo_luettelo:
         self.email = email
         self.puhelinnumero = puhelinnumero
         self.kotiosoite = kotiosoite
-        self.ika = self.syntpvm  - date.today()
+
+    def ika(self):
+        today = date.today()
+        age = today.year - self.syntpvm.year
+
+        if today < date(today.year, self.syntpvm.month, self.syntpvm.day):
+            age -= 1
+        
+        return age
+        
 
 
-man1 = ("Jorma", "Jormakka", date(1985,2,23))
+man1 = Henkilo_luettelo("Jorma", "Jormakka", date(1985,2,23))
 print(date.today())
-print(date(1985,2,3))
-print(date(1985,2,3,)-date.today())
-#print(man1.syntpvm)
-#print(man1.ika)
+print(man1.syntpvm)
+print(man1.ika())
--- Excercise3.py
+++ Excercise3.py
@@ -5,8 +5,11 @@ määritellä kun luodaan oliota. Eläimiä voidaan lisätä myöhemmin luokan
 add_pet metodin avulla."""
 
 class Person:
-    def __init__(self,pets=[]):
-        self.pets = pets
+    def __init__(self,pets=None): #This is a weird syntax...
+        if pets == None:
+            self.pets = []
+        else:
+            self.pets = pets
 
     def add_pet(self, new_pet):
         self.pets.append(new_pet)
@@ -21,4 +24,4 @@ janne.add_pet("cat")
 janne.add_pet("dog")
 
 print("Jannen eläimet: ", janne.pets)
-print("Helenan eläimet: ", helena.pets) # -> "Helenan eläimet:  ['cat', 'dog']", MIKSI ???? Helenalla ei pitäisi olla eläimiä...
+print("Helenan eläimet: ", helena.pets) 
